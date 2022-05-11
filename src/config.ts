dc136b2b2236d8bb52b881e1d16278d23bc584fb
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-11 19:21:21 -0300

    Removes unused env variables from config + adjusting env path

--- src/config.ts
+++ src/config.ts
@@ -2,21 +2,19 @@ import path from "path";
 import dotenv from "dotenv";
 
 // Parsing the env file.
-dotenv.config({ path: path.resolve(__dirname, "./config.env") });
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // Interface to load env variables
 // Note these variables can possibly be undefined
 // as someone could skip these varibales or not setup a .env file at all
 
 interface ENV {
-  NODE_ENV: string | undefined;
   PORT: number | undefined;
   DATABASE_URL: string | undefined;
   ADMIN_EMAIL: string | undefined;
 }
 
 interface Config {
-  NODE_ENV: string;
   PORT: number;
   DATABASE_URL: string;
   ADMIN_EMAIL: string | undefined;
@@ -26,7 +24,6 @@ interface Config {
 
 const getConfig = (): ENV => {
   return {
-    NODE_ENV: process.env.NODE_ENV,
     PORT: process.env.PORT ? Number(process.env.PORT) : undefined,
     DATABASE_URL: process.env.DATABASE_URL,
     ADMIN_EMAIL: process.env.ADMIN_EMAIL,
