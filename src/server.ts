7292270cb1a99546c4533d59ff953761d297e77d
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-11 01:15:52 -0300

    Adds email sender with nodemailer and mailtrap when feedback is sent

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import { prisma } from './prisma';
 import express from 'express';
 import nodemailer from 'nodemailer';
+import config from './config';
 
 const app = express();
 
 app.use(express.json()); // middleware
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "4c1460f39ae0ce",
+    pass: "143a89be9bcf01"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -16,6 +26,18 @@ app.post('/feedbacks', async (req, res) => {
       screenshot,
     }
   })
+
+  transport.sendMail({
+    from: 'Feedget <oi@feedget.com>',
+    to: config.ADMIN_EMAIL,
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 14px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n'),
+  });
 
   return res.status(201).json(feedback);
 })
