6a179adb75dfd9d35711f56c5a0e2f14e4d368c4
Julia Gomes <magalhaesjulia@id.uff.br>
2022-05-11 21:23:28 -0300

    Adds cors to server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
 
+app.use(cors()); // security step to enable only specific front end servers from accessing
 app.use(express.json()); // middleware
 app.use(routes);
 
