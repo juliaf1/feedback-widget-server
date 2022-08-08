8f23a109eb5dd368f6721c82e3d698790e1ff1a0
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-08 18:54:23 -0300

    adds conditional to send screenshot on email + sets image width on email

--- src/services/submit-feedback-service.ts
+++ src/services/submit-feedback-service.ts
@@ -40,7 +40,7 @@ export class SubmitFeedbackService {
         `<div style="font-family: sans-serif; font-size: 14px; color: #111;">`,
         `<p>Tipo do feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
-        `<img src="${screenshot}">`,
+        screenshot ? `<img src="${screenshot}" width="100%">` : ``,
         `</div>`
       ].join('\n'),
     });
